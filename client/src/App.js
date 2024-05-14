4fea9091865d6d337ee756816e69da6ffad1f85d
Roman Chernetsky <misteryo.wins@gmail.com>
2024-05-14 13:55:19 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import {
+  ChakraProvider,
+  Heading,
+  Container,
+  Text,
+  Input,
+  Button,
+  Wrap,
+  Stack, 
+  Image,
+  SkeletonCircle,
+  SkeletonText,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+
+const App = () => {
+  const [image, updateImage] = useState();
+  const [prompt, updatePrompt] = useState();
+  const [loading, updateLoading] = useState();
+
+  const generate = async (prompt) => {
+    updateLoading(true);
+    const result = await axios.get(`http://127.0.0.1:8000/?prompt=${prompt}`);
+    updateImage(result.data);
+    updateLoading(false);
+  };
+
+  return (
+    <ChakraProvider>
+      <Container>
+        <Heading>⭐Генерация изображений⭐</Heading>
+        <Text marginBottom={"10px"}>
+          На этом сайте вы сможете генерировать случайные изображения, 
+          использовав вашу фантазию и слова, занесенные в поле prompt  {" "}
+        </Text>
+
+        <Wrap marginBottom={"10px"}>
+          <Input
+            value={prompt}
+            onChange={(e) => updatePrompt(e.target.value)}
+            width={"350px"}
+          ></Input>
+          <Button onClick={(e) => generate(prompt)} colorScheme={"yellow"}>
+            Generate
+          </Button>
+        </Wrap>
+
+        {loading ? (
+          <Stack>
+            <SkeletonCircle />
+            <SkeletonText />
+          </Stack>
+        ) : image ? (
+          <Image src={`data:image/png;base64,${image}`} boxShadow="lg" />
+        ) : null}
+      </Container>
+    </ChakraProvider>
+  );
+};
+
+export default App;
